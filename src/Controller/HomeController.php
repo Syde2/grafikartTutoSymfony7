28d20779ca8f6459c6079d2a9aa367063db1f5aa
Syde2 <contact@syde2hub.com>
2024-02-20 20:34:09 +0100

    Ajout pico.css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,21 +17,9 @@ class HomeController extends AbstractController
     #[Route('/', name: 'recipe.show' )]
     public function index( RecipeRepository $repository, EntityManagerInterface $em )
     {
-        dd($repository->getTotalTime());
-        $recipes =  $em->getRepository(Recipe::class)->findAll();
-        $repo =  $em->getRepository(Recipe::class);
-        $recipe = new Recipe();
-        $recipe
-        ->setTitle("barbe a Papa")
-        ->setSlug('barbe-papa')
-        ->setContent('mettez du Sucre')
-        ->setDuration(2)
-        ->setCreatedAt(new DateTimeImmutable())
-        ->setUpdatedAt(new DateTimeImmutable());
+        return $this->render('home/index.html.twig', [
+            'controller_name'=>'index'
+        ]);
 
-        $em->persist($recipe);
-        $em->flush();
-
-       return $this->json(['recipes'=> $recipes ]);
     }
 }
