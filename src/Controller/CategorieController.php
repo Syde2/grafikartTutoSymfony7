81f1f85becd3c2e88f69044ebcb40a05419c952d
Syde2 <contact@syde2hub.com>
2024-02-26 21:23:48 +0100

    Ajout User

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Controller;
 
 use App\Entity\Categorie;
@@ -10,9 +9,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
+#[Route('/categorie', ) ]
 class CategorieController extends AbstractController
 {
-    #[Route('/categorie', name: 'categorie')]
+    #[Route('', name:'categorie' )]
     public function index(CategorieRepository $categorieRepository ): Response
     {
         $categories = $categorieRepository->findAll();
@@ -21,7 +22,7 @@ class CategorieController extends AbstractController
         ]);
     }
 
-    #[Route('/categorie/{id}/edit', name: 'categorie.edit')]
+    #[Route('/{id}/edit', name: 'categorie.edit')]
     public function edit(Categorie $categorie, EntityManagerInterface $em  ): Response
     {
 
@@ -30,7 +31,7 @@ class CategorieController extends AbstractController
         ]);
     }
 
-    #[Route('/categorie/{id}/delete', name: 'categorie.delete')]
+    #[Route('/{id}/delete', name: 'categorie.delete')]
     public function delete(Categorie $categorie, EntityManagerInterface $em  ): Response
     {
         $em->remove($categorie);
