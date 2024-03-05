17c489ae11c51dd6f3f3b19c32b7bcf996366138
Syde2 <contact@syde2hub.com>
2024-03-05 19:20:47 +0100

    Register

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\RecipeRepository;
 use App\Entity\Recipe;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use DateTime;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,14 +16,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'index' )]
-    public function index( EntityManagerInterface $em , UserPasswordHasherInterface $hasher )
+    public function index( UserRepository $userRepository )
     {
         $user = $this->getUser();
+        $users = $userRepository->findAll();
         if ($user === null) {
             return $this->redirectToRoute('app_login');
          }
+         dd($users);
          return $this->render('home/index.html.twig', [
-            'controller_name' => 'Index',
+            'users' => $users,
+            'user' => $user->getUserIdentifier()
         ]);;
     }
+
+
 }
